55839e6cb1d6cb5afce5a4362ded44b296cb48cd
haoli <xuhaoli1996@gmail.com>
2026-08-12 21:05:20 +0800

    修改变量名称

--- octpus-spring-boot-starter/src/main/java/com/octpus/spring/gateway/GatewayController.java
+++ octpus-spring-boot-starter/src/main/java/com/octpus/spring/gateway/GatewayController.java
@@ -33,16 +33,16 @@ public class GatewayController {
         MDC.put(TRACE_ID_KEY, traceId);
 
         try {
-            String method = request.getMethod();
+            String serviceName = request.getServiceName();
             String version = request.getVersion();
 
-            log.info("[Octpus] received: method={}, version={}, traceId={}", method, version, traceId);
+            log.info("[Octpus] received: serviceName={}, version={}, traceId={}", serviceName, version, traceId);
 
-            if (method == null || method.isBlank()) {
-                throw new OctpusException(OctpusException.ERR_METHOD_MISSING, "method cannot be empty");
+            if (serviceName == null || serviceName.isBlank()) {
+                throw new OctpusException(OctpusException.ERR_METHOD_MISSING, "serviceName cannot be empty");
             }
 
-            Object result = serviceRouter.route(method, version, request.getData());
+            Object result = serviceRouter.route(serviceName, version, request.getData());
 
             // 如果方法返回的是 GatewayResponse，直接返回；否则包装
             if (result instanceof GatewayResponse) {
